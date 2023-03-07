41892d5aeb73122356a1ea32742377d73e77304e
mixtape115 <g0115renshu@gmail.com>
2023-03-07 22:08:19 +0900

    add encoder

--- ch2/vit.py
+++ ch2/vit.py
@@ -100,5 +100,163 @@ input_layer = VitInputLayer(num_patch_row=2)
 z_0=input_layer(x)
 
 # (2, 5, 384)(=(B, N, D))になっていることを確認
-print(z_0.shape)
+print('z_0 : {}'.format(z_0.shape))
+
+# ---------------------------------------------
+# Self-Atention
+# ------------------------------------------------
+print("========================================================================")
+print("====================== 2-4 Self-Attention ==============================")
+print("========================================================================")
+
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, emb_dim:int=384, head:int=3, dropout:float=0.):
+        """
+        引数:
+            emb_dim: 埋め込み後のベクトルの長さ
+            head: ヘッドの数
+            dropout: ドロップアウト率
+        """
+        super(MultiHeadSelfAttention, self).__init__()
+        self.head = head
+        self.emb_dim = emb_dim
+        self.head_dim = emb_dim // head
+        self.sqrt_dh = self.head_dim**0.5 # D_hの二乗根。qk^Tを割るための係数
+
+        # 入力をq,k,v に埋め込むための線形層
+        self.w_q = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.w_k = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.w_v = nn.Linear(emb_dim, emb_dim, bias=False)
+
+        # 式(7)にはないが実装ではDropoutも用いる
+        self.attn_drop = nn.Dropout(dropout)
+
+        # MHSAの結果を出力に埋め込むたための線形層
+        ## 式(10)にはないが実装ではドロップアウトも用いる
+        self.w_o = nn.Sequential(
+            nn.Linear(emb_dim, emb_dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, z:torch.Tensor) -> torch.Tensor:
+        """
+        引数:
+            z: MHSAへの入力。形状は(B, N, D)
+                B: バッチサイズ、N: トークンの数、D: 埋め込みベクトルの長さ
+
+        返り値:
+            out: MHSAの出力。形状は(B, N, D)。式(10)
+                 B: バッチサイズ、N: トークンの数, D: 埋め込みベクトルの長さ
+        """
+
+        batch_size, num_patch, _ = z.size()
+
+        # 埋め込み 式6
+        ## (B, N,D) -> (B, N, D)
+        q = self.w_q(z)
+        k = self.w_k(z)
+        v = self.w_v(z)
+
+        # q,k,vをヘッドに分割。式(10)
+        ## まずベクトルをヘッドの個数(h)に分ける
+        ## (B,N,D) -> (B,N,D//h)
+        print("Before view {}".format(q.size()))
+        q = q.view(batch_size, num_patch, self.head, self.head_dim)
+        print("After view {}".format(q.size()))
+        k = k.view(batch_size, num_patch, self.head, self.head_dim)
+        v = v.view(batch_size, num_patch, self.head, self.head_dim)
+
+        ## Self-Attwntionができるように
+        ## (バッチサイズ、ヘッド、トークン数、パッチのベクトル)の形に変更する
+        ## (B,N,h,D//h) -> (B,h,N,D//h)
+        q = q.transpose(1,2)
+        k = k.transpose(1,2)
+        v = v.transpose(1,2)
+
+        # 内積
+        ## (B, h, N, D//h) -> (B, h, D//h, N)
+        k_T = k.transpose(2, 3)
+        ## (B, h, N, D//h) x (B, h, D//h, N) -> (B, h, N, N) 
+        dots = (q @ k_T) / self.sqrt_dh
+        ## 列方向にソフトマックス関数
+        attn = F.softmax(dots, dim=-1)
+        ## ドロップアウト
+        attn = self.attn_drop(attn)
+
+        # 加重和 式(8)
+        ## (B, h, N, N) x (B, h, N, D//h) -> (B, h, N, D//h) 
+        out = attn @ v
+        ## (B, h, N, D//h) -> (B, N, h, D//h)
+        out = out.transpose(1, 2)
+        ## (B, N, h, D//h) -> (B, N, D)
+        print("Before reshape {}".format(out.size()))
+        out = out.reshape(batch_size, num_patch, self.emb_dim)
+        print("After reshape {}".format(out.size()))
+
+        # 出力層 [式(10)]
+        ## (B, N, D) -> (B, N, D) 
+        out = self.w_o(out) 
+        
+        return out
+
+mhsa = MultiHeadSelfAttention()
+out = mhsa(z_0) #z_0は2-2節のz_0=input_layer(x)で、形状は(B, N, D)
+
+# (2, 5, 384)(=(B, N, D))になっていることを確認 
+print(out.shape)
+
+# ----------------------------
+# 2-5 Encoder
+# ----------------------------
+print("=======2-5 Encoder=======")
+
+class VitEncoderBlock(nn.Module): 
+    def __init__(self, emb_dim:int=384, head:int=8, hidden_dim:int=384*4, dropout: float=0.):
+        """
+        引数:
+            emb_dim: 埋め込み後のベクトルの長さ
+            head: ヘッドの数
+            hidden_dim: Encoder BlockのMLPにおける中間層のベクトルの長さ 
+                        原論文に従ってemb_dimの4倍をデフォルト値としている
+            dropout: ドロップアウト率
+        """
+        super(VitEncoderBlock, self).__init__()
+        # 1つ目のLayer Normalization [2-5-2項]
+        self.ln1 = nn.LayerNorm(emb_dim)
+        # MHSA [2-4-7項]
+        self.msa = MultiHeadSelfAttention(
+        emb_dim=emb_dim, head=head,
+        dropout = dropout,
+        )
+        # 2つ目のLayer Normalization [2-5-2項] 
+        self.ln2 = nn.LayerNorm(emb_dim)
+        # MLP [2-5-3項]
+        self.mlp = nn.Sequential( 
+            nn.Linear(emb_dim, hidden_dim), 
+            nn.GELU(),
+            nn.Dropout(dropout), 
+            nn.Linear(hidden_dim, emb_dim), 
+            nn.Dropout(dropout)
+        )
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        """ 
+        引数:
+            z: Encoder Blockへの入力。形状は、(B, N, D)
+                B: バッチサイズ、N:トークンの数、D:ベクトルの長さ
+        返り値:
+            out: Encoder Blockへの出力。形状は、(B, N, D)。[式(10)]
+                B:バッチサイズ、N:トークンの数、D:埋め込みベクトルの長さ 
+        """
+        # Encoder Blockの前半部分 [式(12)] 
+        out = self.msa(self.ln1(z)) + z
+        # Encoder Blockの後半部分 [式(13)] 
+        out = self.mlp(self.ln2(out)) + out 
+        return out
+
+vit_enc = VitEncoderBlock()
+print('z_0 : {}'.format(z_0.shape))
+z_1 = vit_enc(z_0) # z_0は2-2節のz_0=input_layer(x)で、形状は(B, N, D)
+
+# (2, 5, 384)(=(B, N, D))になっていることを確認 
+print('z_1 : {}'.format(z_1.shape))
 
